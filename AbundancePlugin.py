e2e62637a0ba1b3e020da950fe1a1c44dae6c91e
Trevor Cickovski <tcickovs@castalia.cs.fiu.edu>
2020-07-03 09:32:17 -0400

    Fixed to handle quotes

--- AbundancePlugin.py
+++ AbundancePlugin.py
@@ -3,9 +3,14 @@ import sys
 class AbundancePlugin:
    def input(self, filename):
       filestuff = open(filename, 'r')
-      firstline = filestuff.readline() # Read first line
+      firstline = filestuff.readline().strip() # Read first line
       self.taxa = firstline.split(',')
       self.taxa.remove(self.taxa[0])  # Remove placeholder
+
+      # Possibly remove quotes
+      for i in range(len(self.taxa)):
+         if self.taxa[i][0] == '\"':
+            self.taxa[i] = self.taxa[i][1:len(self.taxa[i])-1]
 
       self.lines = []
       for line in filestuff:
@@ -24,8 +29,8 @@ class AbundancePlugin:
             self.sums[self.taxa[i]] += float(elements[i])
       for key in self.sums:
          self.sums[key] /= len(self.lines)
-         if (self.sums[key] != 0):
-            self.tuples.append((self.sums[key], key))
+         #if (self.sums[key] != 0):
+         self.tuples.append((self.sums[key], key))
       self.tuples.sort()
       self.tuples.reverse()
 
